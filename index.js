8afeb4363ee617cebfb3ce8ce4f09db32373300f
ghisolfi-andrea <115536229+ghisolfi-andrea@users.noreply.github.com>
2023-04-12 10:53:16 +0000

    ho fatto modifiche

--- index.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('click', (e) => {
-    const dropdowns = document.querySelectorAll('nav ul ul');
-    dropdowns.forEach(dropdown => {
-      if (!dropdown.contains(e.target)) {
-        dropdown.style.display = 'none';
-      }
-    });
-  });
